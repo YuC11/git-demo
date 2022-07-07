1811afe3b71220ff32a12e157e453126b4c9ce6c
yc <1257010256@qq.com>
2022-07-07 17:24:40 +0800

    master 2

---
+++ src/main/java/com/yc/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.yc.git;
+
+
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("master2");
+
+    }
+
+}
